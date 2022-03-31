25d44410499f541d772806d57206c70b0c67bbaa
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-31 13:35:39 -0400

    fix error

--- main_app/models.py
+++ main_app/models.py
@@ -3,8 +3,8 @@ import datetime
 
 # Create your models here.
 class Note(models.Model):
-    title: models.CharField(max_length=100)
-    text: models.CharField(max_length=300)
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=300)
     created_at = models.DateTimeField(default= datetime.datetime.now())
    
     class Meta:
