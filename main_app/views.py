68feffe865ca08ebb90674c89a6c4370422b5b3d
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-31 14:21:24 -0400

    add success_url on create note

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Note
 
 # Create your views here.
 
 def home (request):
     return  render(request, 'home.html')
+
+
+class NoteCteate(CreateView):
+    model = Note
+    fields = ['title', 'text']
+    success_url = '/'
+    
