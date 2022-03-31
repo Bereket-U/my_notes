7ca8a76e6eec6faf924fb7eabbed8e9a0bb1be30
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-31 13:30:06 -0400

    add created_at field on model and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Note(models.Model):
     title: models.CharField(max_length=100)
     text: models.CharField(max_length=300)
+    created_at = models.DateTimeField(default= datetime.datetime.now())
+   
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.text
