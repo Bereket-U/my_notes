d1ba83326cc6b928788254ee00afd20e0c8a0fc6
Bereket Urgessa <bereketadugna15@gmail.com>
2022-04-01 00:49:41 -0400

    add notes detail path

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('notes/create', views.note_create.as_view(), name='notes_create'),
-    
+    path('notes/<int:note_id>',views.note_detail, name='note_detail' )
 ]
